6f5cf3ce1d87ec4e357c59694556eb74ae3f957c
Medo <dimoski.ivan@gmail.com>
2016-12-16 23:05:00 +0100

    Replace immersive mode with no limits layout

--- app/src/main/java/com/medo/tweetspie/main/viewer/ViewerActivity.java
+++ app/src/main/java/com/medo/tweetspie/main/viewer/ViewerActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.view.ViewPager;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.medo.tweetspie.R;
 import com.medo.tweetspie.base.BaseActivity;
@@ -57,21 +59,6 @@ public class ViewerActivity extends BaseActivity implements ViewerContract.View 
   }
 
   @Override
-  public void onWindowFocusChanged(boolean hasFocus) {
-
-    super.onWindowFocusChanged(hasFocus);
-    if (hasFocus) {
-      getWindow().getDecorView().setSystemUiVisibility(
-              View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                      | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                      | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                      | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                      | View.SYSTEM_UI_FLAG_FULLSCREEN
-                      | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-    }
-  }
-
-  @Override
   protected void inject(@NonNull AppComponent appComponent) {
 
     DaggerUserComponent.builder()
@@ -83,6 +70,10 @@ public class ViewerActivity extends BaseActivity implements ViewerContract.View 
 
   @Override
   public void initUi() {
+
+    final Window window = getWindow();
+    window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
     setContentView(R.layout.activity_viewer);
     ButterKnife.bind(this);
